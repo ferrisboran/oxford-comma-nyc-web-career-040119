3be41a45c3b7515b5aebd198fbfcd0b15ea31699
Ferris Boran <ferris.boran@gmail.com>
2019-03-11 19:54:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
 	last = []
-  if array.length == 1
-    puts array
-  else
-  	last << "and #{array.pop}"
-  	array.concat(last)
-  end
-	puts array.join(', ')
+	if array.length == 1
+		puts array
+	elsif array.length == 2
+		puts array.join(' and ')
+	else
+		last << "and #{array.pop}"
+		array.concat(last)
+		puts array.join(', ')
+	end
 end
